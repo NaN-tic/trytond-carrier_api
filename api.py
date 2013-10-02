64c4549e18c4c40fa88834ff4bfea8ca6a7495f0
resteve <resteve@zikzakmedia.com>
2013-10-02 18:56:21 +0200

    pyflakes

--- api.py
+++ api.py
@@ -2,7 +2,6 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields, ModelSQL, ModelView
-from trytond.pool import Pool
 from trytond.transaction import Transaction
 from trytond.pyson import Eval
 
