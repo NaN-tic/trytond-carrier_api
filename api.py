d4faf951293b2ade6bb2dac1e033216b61702e6d
resteve <resteve@zikzakmedia.com>
2014-03-25 11:58:51 +0100

    Encoding utf8 api py

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 #This file is part carrier_api module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
